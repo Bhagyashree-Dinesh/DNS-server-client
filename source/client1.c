86e6618ab43da55ab2a30d7d6ee937b7bb84c90a
Bhagyashree-Dinesh <41187221+Bhagyashree-Dinesh@users.noreply.github.com>
2018-07-25 14:59:07 +0530

    Update client1.c

--- source/client1.c
+++ source/client1.c
@@ -1,4 +1,8 @@
-// Client side implementation of UDP client-server model
+
+/* This is Client side implementation of UDP client-server model. A DNS request submitted at the client side 
+will be searched in the local cache of the client, which is implemented as a linked list. If a match is found, the result
+is outputted. Else, a request is sent to the server */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -16,17 +20,25 @@
 #define PORT     8080
 #define MAXLINE 1024
 #define MIN_COUNT 1
-#define MAX_NODES 5
+#define MAX_NODES 5 
+/*The linked list is allowed to have only MAX_NODES number of entries */
+/*If the number of entries exceed, the least frequently used ones are deleted*/
 
 
-struct node
+/*This structure specifies the fields of every entry allowed in the linked list
+at the client side. ip and dname refer to IP address and Domain name. 'count' keeps 
+track of number of times a particular domain name DNS query happens. 'seconds' acts as 
+time stamp for each entry, which is refreshed every time a query pertaining to it happens. */
+
+struct node 
 {
-char ip[100];
-char dname[100];
-int count;
-time_t seconds;
-struct node* next;
+	char ip[100];
+	char dname[100];
+	int count;
+	time_t seconds;
+	struct node* next;
 };
+
 
 struct node* readnode(struct node* ,char [],char []);
 void displaynode(struct node* );
@@ -34,107 +46,109 @@ int searchnode(struct node*,char []);
 struct node* deletenode(struct node*);
 int node_count;
 
-// Driver code
 
 int main() 
 {
-    char dns_query[100];
+    char dns_query[100];  /* String holding message from client to server*/
     int sockfd;
     struct sockaddr_in  servaddr;
-    char buffer[MAXLINE];
+    char buffer[MAXLINE];  /* String holding message from server to client*/
     struct node* head;
     int c;
     
-   
-	while(1)
-	{
+/*In the while loop, 2 options are presented. First one is for DNS query
+and option 2 is for exit */
+	
+while(1)
+{
 	printf("\n\t MAIN MENU\n1.Send DNS Query: \n2.Exit :\n  ");
-    printf("\nChoose an option : ");
+    	printf("\nChoose an option : ");
 	scanf("%d",&c);
-	switch(c)
+		switch(c) 
 	{
-	case 1:{
-    printf("Enter Domain Name :");
-    scanf("%s",dns_query);
-	int flag;
-    flag=searchnode(head,dns_query);
+		case 1:{
+   	 	printf("Enter Domain Name :");
+   		 scanf("%s",dns_query);
+		int flag;
+    		flag=searchnode(head,dns_query);
     
-    // Creating socket file descriptor
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) 
-	{
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+   		 /* Creating socket file descriptor */
+    		if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) 
+			{
+        	perror("socket creation failed");
+        	exit(EXIT_FAILURE);
+    			}
  
-    memset(&servaddr, 0, sizeof(servaddr));
+   		 memset(&servaddr, 0, sizeof(servaddr));
      
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    		/*Filling server information */
+   		servaddr.sin_family = AF_INET;
+    		servaddr.sin_port = htons(PORT);
+    		servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
      
-    int n, len;
-    if(!flag) //server request in case of no mapping in client LL
-	{
-    sendto(sockfd, (const char *)dns_query, strlen(dns_query),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    printf("\n Dns request sent.\n");
-    len=sizeof(struct sockaddr_in)  ;
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-	if(n<0)
-	fprintf(stderr,"\n error in receiving");
-    buffer[n] = '\0';
-    printf("\nServer : %s\n", buffer);
-    if(buffer[n-2]!='!')
-    {	    
-	if(node_count>=MAX_NODES)
-	   {head=deletenode(head); 
-            
-}
+    		int n, len;
+    		if(!flag) /* server request in case of no mapping in client linked list */
+		{	/*Send a request to server*/
+   			 sendto(sockfd, (const char *)dns_query, strlen(dns_query),
+     			 MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+           	  	 sizeof(servaddr));
+  			  printf("\n Dns request sent.\n");
+  			  len=sizeof(struct sockaddr_in)  ;
+			/*Receive the server reply,i.e IP address in the variable buffer*/
+  			  n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+             		   MSG_WAITALL, (struct sockaddr *) &servaddr,
+               			 &len);
+			if(n<0)
+			fprintf(stderr,"\n error in receiving");
+   			 buffer[n] = '\0';
+   			 printf("\nServer : %s\n", buffer);
+   			 if(buffer[n-2]!='!')
+   				 {	    
+					if(node_count>=MAX_NODES)
+	 				  { head=deletenode(head); }
 
-	head=readnode(head,dns_query,buffer);
-    displaynode(head);
-    close(sockfd);
-    }
-    }
+	                 head=readnode(head,dns_query,buffer);
+   			 displaynode(head);
+   			 close(sockfd);
+   				 }
+  		  }
 
 	}break;
 	
 case 2 : exit(0);
-    } //switch
-    } //while 
+    } 
+    } 
 	return 0;
 
-} //Main
+} 
 
+/*Function to create a new node with domain name 
+and IP address and add it to linked list*/
 struct node* readnode(struct node* head,char name[100],char ipaddr[100])
 {
     struct node* cur=(struct node*)malloc(sizeof(struct node));
-	if(head==NULL)
+	if(head==NULL) /*if linked list is empty*/
 	{
 	strcpy(cur->ip,ipaddr);
 	strcpy(cur->dname,name);
-    cur->seconds=time(NULL);
-    cur->count=1;
+        cur->seconds=time(NULL);
+        cur->count=1;
 	cur->next=NULL;	
 	head=cur;
 	}
 	
-	else
+	else  /*If linked list is empty, append new node at the end*/
 	{
 	struct node* temp;
 	temp=head;
 	while(temp->next!=NULL)
-	{
-	temp=temp->next;
-	}
+		 {
+		temp=temp->next;
+		}
 	strcpy(cur->ip,ipaddr);
 	strcpy(cur->dname,name);
-    cur->seconds=time(NULL);
-    cur->count=1;
+   	 cur->seconds=time(NULL);
+    	cur->count=1;
 	cur->next=NULL;	
 	temp->next=cur;
 	}
@@ -142,6 +156,8 @@ node_count++;
 return head;
 }
 
+
+/*Function to display the linked list*/
 void displaynode(struct node* head)
 {	
 	struct node* temp;
@@ -154,31 +170,37 @@ void displaynode(struct node* head)
 	}
 }
 
+
+/*Function to serach for a DNS request in client cache*/
+/*This function returns a flag which is equal to 1 incase a match is found,
+else it returns 0 */
 int  searchnode(struct node* head,char name[100])
 {       
 	
 	struct node* temp;
 	temp=head;
 	int flag=0;
-    int len;
+        int len;
 	
 	while(temp!=NULL)
 	{
-	if(!strcmp(name,temp->dname))
-	{ 
-           temp->count++; //Number of times DNS query is done
-           temp->seconds=time(NULL);
-    printf("\n\nHost IP: %s\n",temp->ip);
-	flag=1;
-	break;
-	}	
+	if(!strcmp(name,temp->dname)) /*if match is found, increase count, refresh the timer and set the flag */
+		{ 
+         	temp->count++; //Number of times DNS query is done for a particular Domain name
+          	temp->seconds=time(NULL);
+   		printf("\n\nHost IP: %s\n",temp->ip);
+		flag=1;
+		break;
+		}	
 	temp=temp->next;	
 	}
         
        return flag;
 }
 
-
+/*This function deletes a node that has been in the local cache 
+for more than 50 seconds without being searched second time. It returns
+the head node after deleting the least frequently used node*/
  struct node* deletenode(struct node* head)
 {
 	 
@@ -204,7 +226,7 @@ int  searchnode(struct node* head,char name[100])
 			if ((time_now-(s->next->seconds)>=50)&&(!flag))
 			{
 			 small=s;
-             flag=1;
+            		 flag=1;
 			}
 			if ((time_now-(s->next->seconds)>=50)&&(flag))	
 			{
@@ -220,8 +242,7 @@ int  searchnode(struct node* head,char name[100])
 	 prev=small;
 	 prev->next=ptr->next;
 	 free(ptr);   
-
-  return head;
+	 return head;
 
 }
 
